8b843cb0840c5fda21ceccfdc110a2735df7f15e
mikewainaina <michael.wainaina@student.moringaschool.com>
2024-06-04 16:14:19 +0300

    lab done

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"([A-Z][a-z]*([ \-']||$))+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\(?\d{3}\)?[- ]?\d{3}-?\d{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[a-zA-Z][\w.]+@\w+.com"
 email_regex = re.compile(email_address)
